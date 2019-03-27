1bf2e540735cdbc1706807d0593f4229a7ea7b8f
Zech Drinkhall <zechdrink@aol.com>
2019-03-27 15:09:21 -0700

    completed get students by cohort endpoint

--- index.js
+++ index.js
@@ -66,6 +66,45 @@ server.put('/api/cohorts/:id', (req, res) => {
     .catch(err => res.status(500).json(err));
 })
 
+//get students by cohort
+
+server.get('/api/cohorts/:id/students', async (req, res) => {
+  const id = req.params.id;
+try {
+    const students = await db
+    .select('*')
+    .from('cohorts')
+    .where({'cohorts.id': id})
+    .join('students', {'cohorts.id': 'students.cohort_id'})
+    res.status(200).json(students);
+} catch (error) {
+    res.status(500).json(error);
+}
+});
+
+//STUDENT ENDPOINTS BEGIN
+
+server.get('/api/students', (req, res) => {
+  db('students').then(students => {
+    res.status(200).json(students);
+  }).catch(err => {res.status(500).json(err)});
+})
+
+
+//Get Student by ID
+server.get('/api/students/:id', (req, res) =>{
+  const studentID = req.params.id;
+  db('students')
+    .where({ id: studentID })
+    .first()
+    .then(student => {
+      res.status(200).json(student);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
+})
+
 
 
 const port = 3300;
